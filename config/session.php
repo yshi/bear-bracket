6dc98fe7a47715d9503b0fce35e6627eec96a7c9
Nick Evans <nick.evans@northwestern.edu>
2024-09-04 19:37:36 -0500

    Long session

--- config/session.php
+++ config/session.php
@@ -32,7 +32,7 @@ return [
     |
     */
 
-    'lifetime' => env('SESSION_LIFETIME', 120),
+    'lifetime' => env('SESSION_LIFETIME', 2592000),
 
     'expire_on_close' => env('SESSION_EXPIRE_ON_CLOSE', false),
 

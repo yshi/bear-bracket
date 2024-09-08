348a0fa3207904b84c928b62050a65cdc6134a16
Nick Evans <nick.evans@northwestern.edu>
2024-09-08 11:38:08 -0500

    Less ugly colour

--- resources/views/components/tournament/stat/card.blade.php
+++ resources/views/components/tournament/stat/card.blade.php
@@ -1,5 +1,5 @@
 @props(['label', 'slot'])
-<div class="w-40 text-right bg-gray-200 dark:bg-gray-700 px-4 py-2 rounded shadow-xl flex flex-col justify-around">
+<div class="w-40 text-right bg-stone-50 dark:bg-gray-700 px-4 py-2 rounded shadow-xl flex flex-col justify-around">
     <div class="text-4xl flex justify-end">
         {{ $slot }}
     </div>
